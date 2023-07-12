295595d6dda78cac398b27951d79b1be4b62c31b
hongjaewon62 <hongjaewon0602@gmail.com>
2023-07-12 17:39:57 +0900

    UI 클릭 기능 추가

--- Assets/Scripts/Controller/Grab.cs
+++ Assets/Scripts/Controller/Grab.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class GrabWeapon : MonoBehaviour
+public class Grab : MonoBehaviour
 {
     private bool isGrabbing = false;
-    public Transform GrabPositon;
-    private GameObject Weapon;
+    public Transform leftGrabPositon;
+    public Transform rightGrabPosition;
+    private GameObject weapon;
     private LineRenderer lineRenderer;
 
     private void Awake()
@@ -38,43 +39,43 @@ public class GrabWeapon : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
-        Weapon = other.gameObject;
+        weapon = other.gameObject;
 
         if(other.CompareTag("Weapon"))
         {
             if(isGrabbing)      //만약 잡는다면
             {
                 //잡히는 Weapon의 위치 값을 손에 맞춤
-                Weapon.transform.position = GrabPositon.transform.position;
-                Weapon.transform.rotation = GrabPositon.transform.rotation;
+                weapon.transform.position = rightGrabPosition.transform.position;
+                weapon.transform.rotation = rightGrabPosition.transform.rotation;
 
                 //Weapon을 손의 하위 객체로 지정
-                Weapon.transform.SetParent(GrabPositon);
+                weapon.transform.SetParent(rightGrabPosition);
 
                 //손에 고정하기 위해 iskinematic 활성
-                Weapon.GetComponent<Rigidbody>().isKinematic = true;
+                weapon.GetComponent<Rigidbody>().isKinematic = true;
             }
 
             else if(!isGrabbing)        //놓는다면
             {
                 //Weapon을 손의 하위 객체에서 해제
-                Weapon.transform.SetParent(null);
+                weapon.transform.SetParent(null);
 
                 //손에 놓기 위해 iskinematic 비활성
-                Weapon.GetComponent<Rigidbody>().isKinematic = false;
+                weapon.GetComponent<Rigidbody>().isKinematic = false;
             }
         }
     }
 
     private void ItemGrab()
     {
-        Debug.DrawRay(transform.position, transform.forward * 10, Color.red);
+        Debug.DrawRay(leftGrabPositon.position, leftGrabPositon.forward * 10, Color.red);
         //Debug.DrawRay(GrabPositon.position, GrabPositon.forward * 10, Color.blue);
 
         // 라인 시작점
-        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(0, leftGrabPositon.position);
         // 라인 종료점
-        lineRenderer.SetPosition(1, transform.position + (transform.forward * 10));
+        lineRenderer.SetPosition(1, leftGrabPositon.position + (leftGrabPositon.forward * 10));
         lineRenderer.enabled = true;
         RaycastHit hit;
 
@@ -85,8 +86,8 @@ public class GrabWeapon : MonoBehaviour
             {
                 if (OVRInput.GetUp(OVRInput.RawButton.LHandTrigger))        // 버튼 뗐을때
                 {
-                    hit.transform.position = GrabPositon.transform.position;
-                    hit.transform.SetParent(GrabPositon);
+                    hit.transform.position = leftGrabPositon.transform.position;
+                    hit.transform.SetParent(leftGrabPositon);
                     hit.transform.GetComponent<Rigidbody>().isKinematic = true;
                     Destroy(hit.transform.gameObject, 1f);                  // 삭제
                 }
