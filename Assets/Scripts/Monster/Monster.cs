ccc1b432896ff2217c91dff5c79273cc3ee81154
RedOprea <hyunsim01@naver.com>
2023-07-20 00:22:00 +0900

    체력에 따른 몬스터 체력 변경 및 기타 수정
    1. 체력 감소 즉시 체력 슬라이더에 반영
    2. 해당 몬스터 체력 외부 스크립트에서 확인 할 수 있도록 수정
    3. 예외가 발생할 수 있는 부분 예외 처리

--- Assets/Scripts/Monster/Monster.cs
+++ Assets/Scripts/Monster/Monster.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.AI;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Monster : MonoBehaviour
 {
@@ -11,8 +12,11 @@ public class Monster : MonoBehaviour
     private bool isMovingToDestination = false; // 이동 중인지 여부를 나타내는 변수
     NavMeshAgent agent; // 길을 찾아서 이동할 에이전트
 
-    public MonsterScriptable monsterData; // 몬스터 데이터 스크립터블 객체
-    private int currentHealth; // 현재 체력
+    // 체력 관련 변수
+    public MonsterScriptable monsterData;               // 몬스터 데이터 스크립터블 객체
+    public int currentHealth { get; private set; }      // 현재 체력 (외부에서 읽기 허용)
+
+    private Slider hpSlider;                            // 체력 슬라이더
 
     // 외형 카테고리들을 저장할 배열의 배열
     public GameObject[][] appearanceOptions;
@@ -25,7 +29,12 @@ public class Monster : MonoBehaviour
 
     private void Awake()
     {
-        agent = GetComponent<NavMeshAgent>(); // 게임이 시작되면 게임 오브젝트에 부착된 NavMeshAgent 컴포넌트를 가져와서 저장
+        agent = GetComponent<NavMeshAgent>();           // 게임이 시작되면 게임 오브젝트에 부착된 NavMeshAgent 컴포넌트를 가져와서 저장
+        hpSlider = GetComponentInChildren<Slider>();    // 몬스터에서 hp 슬라이더를 찾음
+
+        // 몬스터에 몬스터 슬라이더가 존재하는지 확인
+        if (hpSlider == null)
+            Debug.Log("Error (Monster Slider) : 몬스터에 체력 바가 존재하지 않습니다.");
     }
     private void Start()
     {
@@ -51,6 +60,12 @@ public class Monster : MonoBehaviour
 
     public void SetTrapped(float duration)
     {
+        if (duration < 0)
+        {
+            Debug.Log("Error (Unacceptable Value) : 스턴 길이는 음수가 될 수 없습니다.");
+            return;
+        }
+
         if (!isTrapped)
         {
             isTrapped = true;
@@ -66,13 +81,21 @@ public class Monster : MonoBehaviour
         yield return new WaitForSeconds(trapDuration); // 몬스터 정지
         isTrapped = false;
     }
+
     public void TakeDamage(int damage) // 데미지 받는 코드
     {
-        currentHealth -= damage; // 현재 체력에서 데미지 만큼 빼는 코드
-        if (currentHealth <= 0) // 체력 0 이하시 작동
+        currentHealth -= damage;    // 현재 체력에서 데미지 만큼 빼는 코드
+
+        // 체력 0 이하시 작동
+        if (currentHealth <= 0)
         {
+            hpSlider.value = 0;
             Die(); 
         }
+
+        // 만약 캐릭터가 체력이 남아있을 경우
+        else
+            hpSlider.value = ((float)currentHealth / monsterData.maxHp) * 100;  // 현재 체력을 슬라이더에 반영
     }
 
     private void Die()// 몬스터가 죽었을 때 호출
