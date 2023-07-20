7d7a9726c7733953f279c1ef27ed510ee6c0e030
hongjaewon62 <hongjaewon0602@gmail.com>
2023-07-20 17:21:43 +0900

    시계 수정

--- Assets/Scripts/Controller/WatchUI.cs
+++ Assets/Scripts/Controller/WatchUI.cs
@@ -6,6 +6,8 @@ public class WatchUI : MonoBehaviour
 {
     [SerializeField]
     private GameObject watchUi;
+    //[SerializeField]
+    //private GameObject leftController;
     [SerializeField]
     private Transform cameraTransform;
 
@@ -25,19 +27,29 @@ public class WatchUI : MonoBehaviour
 
         // 왼손 위치 감지
 
-        Vector3 leftHandForward = leftHandRotation * Vector3.forward;
+        //Vector3 leftHandForward = leftHandRotation * Vector3.forward;
+        Vector3 cameraForward = cameraTransform.forward;
+
+        watchUi.transform.position = cameraTransform.position + cameraForward * 1f;
+        watchUi.transform.rotation = Quaternion.LookRotation(cameraForward);
+
+        //watchUi.transform.position = leftController.transform.position + new Vector3(0, 0.5f, 0.3f);
+        //watchUi.transform.rotation = Quaternion.Euler(watchUi.transform.rotation.x + leftController.transform.rotation.x, watchUi.transform.rotation.y + leftController.transform.rotation.y, watchUi.transform.rotation.z + leftController.transform.rotation.z);
+
 
         //Debug.Log(leftHandRotation.eulerAngles.z);
-        Debug.Log(leftHandForward);
+        //Debug.Log(leftHandForward);
         // 손목을 돌렸을 때 UI 활성화
-        if (leftHandRotation.eulerAngles.z > 240f && leftHandRotation.eulerAngles.z < 275f && leftHandForward.x > 0.65f && leftHandForward.x < 1f && !uiActive)
+        //&& leftHandForward.x > 0.65f && leftHandForward.x < 1f 
+        if (leftHandRotation.eulerAngles.z > 200f && leftHandRotation.eulerAngles.z < 300f && !uiActive)
         {
             watchUi.SetActive(true);
             uiActive = true;
         }
 
         // 손목을 원래 위치로 되돌렸을 때 UI 비활성화
-        if ((leftHandRotation.eulerAngles.z <= 240f || leftHandRotation.eulerAngles.z >= 275f) || (leftHandForward.x <= 0.65f || leftHandForward.x >= 1f) && uiActive)
+        // || (leftHandForward.x <= 0.65f || leftHandForward.x >= 1f) 
+        if ((leftHandRotation.eulerAngles.z <= 200f || leftHandRotation.eulerAngles.z >= 300f) && uiActive)
         {
             watchUi.SetActive(false);
             uiActive = false;
