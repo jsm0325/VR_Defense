9e7a0f221077fda024425efdad390499fd8d268b
hongjaewon62 <hongjaewon0602@gmail.com>
2023-08-08 22:30:47 +0900

    미니맵 UI 추가

---
+++ Assets/Scripts/UI/CopyPosition.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyPosition : MonoBehaviour
+{
+    [SerializeField]
+    private bool x, y, z;       // true¸é targetÀÇ ÁÂÇ¥ false¸é ÇöÀç ÁÂÇ¥ »ç¿ë
+    [SerializeField]
+    private Transform target;
+
+    private void Update()
+    {
+        if(!target)
+        {
+            return;
+        }
+
+        transform.position = new Vector3(
+            (x ? target.position.x : transform.position.x),
+            (y ? target.position.y : transform.position.y),
+            (z ? target.position.z : transform.position.z));
+    }
+}
