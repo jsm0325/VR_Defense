87df359a8d51a8655dd4deab7d36f893ce2fbf4b
BADBED <705junho@gmail.com>
2023-09-09 17:51:47 +0900

    billboard 수정 / 침대 - 게임종료 / 문 - 게임 시작 구현

--- Assets/Scripts/UI/Billboard.cs
+++ Assets/Scripts/UI/Billboard.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class Billboard : MonoBehaviour
 {
     // 카메라 바라보고 있게 하는 기능 어디서든 볼 수 있게
+
+    public Transform cam;
+
+
     void Update()
     {
-        transform.LookAt(Camera.main.transform);
+        transform.LookAt(cam);
     }
 }
