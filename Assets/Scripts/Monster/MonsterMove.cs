cd1e68f93b3e5262b845e8448fbd3d9cafba59af
jsm0325 <jsm961207@naver.com>
2023-09-16 21:41:27 +0900

    아기 고양이 아이템 작동안하는 오류 수정

--- Assets/Scripts/Monster/MonsterMove.cs
+++ Assets/Scripts/Monster/MonsterMove.cs
@@ -76,7 +76,7 @@ public class MonsterMove : MonoBehaviour
             agent.SetDestination(finalTarget.transform.position);
         }
         // 목적지와의 거리 
-        if (agent.remainingDistance < 1f) // 목적지에 도착하면 동작함
+        if (agent.remainingDistance < 1f && agent.destination == randomTarget) // 목적지에 도착하면 동작함
         {
             agent.SetDestination(finalTarget.transform.position);
             enteredZone = false;
