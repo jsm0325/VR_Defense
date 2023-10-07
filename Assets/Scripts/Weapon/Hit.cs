15ce5e7c5046669436d43f0e89d6eedc23b4e0ed
badbed7 <55682211+badbed7@users.noreply.github.com>
2023-10-07 10:20:38 +0900

    무기 이팩트
    무기별 이팩트 적용 및 몬스터 공격 시 이팩트 활성

--- Assets/Scripts/Weapon/Hit.cs
+++ Assets/Scripts/Weapon/Hit.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class Hit : MonoBehaviour
 {
     public WeaponScriptable WeaponData;
+    public GameObject hitEffects;
+    public Transform EffectPos;
 
+    
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Monster")
         {
             collision.gameObject.GetComponent<Monster>().TakeDamage(WeaponData.AttackDamage, transform.position, WeaponData.KnockBack,false);
+            GameObject spawnedHit = Instantiate(hitEffects, EffectPos);
+            Debug.Log(spawnedHit.transform.position);
             
         }
     }
